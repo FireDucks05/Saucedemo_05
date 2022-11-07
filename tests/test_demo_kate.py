5d0ab485ac5031fb3803875963f21337962120ad
KateFox <52407432+rgirl0801@users.noreply.github.com>
2022-11-07 12:04:39 -0500

    add maximize window

--- tests/test_demo_kate.py
+++ tests/test_demo_kate.py
@@ -8,10 +8,10 @@ from selenium.webdriver.chrome.service import Service
 
 def test_kate():
     options = webdriver.ChromeOptions()
-    options.add_argument("--window-size=800,600")
     options.headless = True
     browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     browser.get('https://www.selenium.dev/downloads')
+    browser.maximize_window()
     browser.find_element(By.XPATH, "//a[contains(@href,'https://github.com/SeleniumHQ/')]").click()
     assert True
     browser.quit()
