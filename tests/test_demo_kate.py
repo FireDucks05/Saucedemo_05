97e9036cb902cb4a7ac30fa2e29915ac119fd09b
KateFox <52407432+rgirl0801@users.noreply.github.com>
2022-11-09 17:30:37 -0500

    refactor

--- tests/test_demo_kate.py
+++ tests/test_demo_kate.py
@@ -1,20 +1,15 @@
-from selenium.webdriver.common.by import By
-import pytest
-
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def test_login_standart():
     options = webdriver.ChromeOptions()
+    options.add_argument("--window-size=800,600")
     options.headless = True
-    browser = webdriver.Firefox()
-    # browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    # browser = webdriver.Chrome('/Users/sweed/PycharmProjects/SweedScripts/venv/lib/python3.10/site-packages/chromedriver_py/chromedriver_mac_arm64')
-    browser.get("https://www.saucedemo.com/")
-    browser.maximize_window()
-    browser.find_element(By.ID, "user-name").send_keys("standard_user")
-    browser.find_element(By.ID, "password").send_keys("secret_sauce")
-    browser.find_element(By.ID, "login-button").send_keys("secret_sauce")
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    browser.get('https://www.selenium.dev/downloads')
+    browser.find_element(By.XPATH, "//a[contains(@href,'https://github.com/SeleniumHQ/')]").click()
+    assert True
     browser.quit()
