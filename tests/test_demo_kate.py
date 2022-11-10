e94664728393f6fb99e0269c4b354ebedb381596
KateFox <52407432+rgirl0801@users.noreply.github.com>
2022-11-09 20:32:19 -0500

    add TC001

--- tests/test_demo_kate.py
+++ tests/test_demo_kate.py
@@ -7,21 +7,17 @@ user_name = 'standard_user'
 password = 'secret_sauce'
 
 
-def setUp(self):
+def test_TC_001():
     service = Service(ChromeDriverManager().install())
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--window-size=1920,1080')
-    self.driver = webdriver.Chrome(service=service, options=chrome_options)
-    self.driver.get('https://www.saucedemo.com/')
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver.get('https://www.saucedemo.com/')
+    driver.find_element(By.ID, 'user-name').send_keys(user_name)
+    driver.find_element(By.ID, 'password').send_keys(password)
+    driver.find_element(By.ID, 'login-button').click()
+    assert 'https://www.saucedemo.com/inventory.html' == driver.current_url
+    driver.quit()
 
 
-def tearDown(self):
-    self.driver.quit()
 
-
-def test_TC_001(self):
-    self.driver.find_element(By.ID, 'user-name').send_keys(user_name)
-    self.driver.find_element(By.ID, 'password').send_keys(password)
-    self.driver.find_element(By.ID, 'login-button').click()
-
-    assert 'https://www.saucedemo.com/inventory.html' == self.driver.current_url
