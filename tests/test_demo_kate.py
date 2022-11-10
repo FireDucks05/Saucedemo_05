2183b445ddfdd1e626ee5406e77d7731c12d39b9
KateFox <52407432+rgirl0801@users.noreply.github.com>
2022-11-09 20:09:10 -0500

    test2

--- tests/test_demo_kate.py
+++ tests/test_demo_kate.py
@@ -6,11 +6,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
 
-def test_kate_first():
+def test_kate_first_add():
     options = webdriver.ChromeOptions()
-    options.add_argument("--window-size=800,600")
     options.headless = True
     browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    browser.maximize_window()
     browser.get('https://www.selenium.dev/downloads')
     browser.find_element(By.XPATH, "//a[contains(@href,'https://github.com/SeleniumHQ/')]").click()
     assert True
