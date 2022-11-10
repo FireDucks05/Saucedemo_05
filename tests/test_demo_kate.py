65182c56555c6b39a427046d83539c595068766f
KateFox <52407432+rgirl0801@users.noreply.github.com>
2022-11-09 19:14:55 -0500

    refactor

--- tests/test_demo_kate.py
+++ tests/test_demo_kate.py
@@ -6,7 +6,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
 
-def test_kate():
+def test_kate_first():
     options = webdriver.ChromeOptions()
     options.add_argument("--window-size=800,600")
     options.headless = True
@@ -17,12 +17,3 @@ def test_kate():
     browser.quit()
 
 
-def test_login_NEstandart():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--window-size=800,600")
-    options.headless = True
-    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    browser.get('https://www.selenium.dev/downloads')
-    browser.find_element(By.XPATH, "//a[contains(@href,'https://github.com/SeleniumHQ/')]").click()
-    assert True
-    browser.quit()
