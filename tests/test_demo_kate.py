17d2900a435b02585d16d7544a4f8aed3a88cdfc
KateFox <52407432+rgirl0801@users.noreply.github.com>
2022-11-09 20:14:22 -0500

    test3

--- tests/test_demo_kate.py
+++ tests/test_demo_kate.py
@@ -1,18 +1,15 @@
-from selenium.webdriver.common.by import By
-import pytest
-
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = webdriver.ChromeOptions()
+options.headless = True
+browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+browser.maximize_window()
 
 
-def test_kate_first_add():
-    options = webdriver.ChromeOptions()
-    options.headless = True
-    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    browser.maximize_window()
+def test_kate_first():
     browser.get('https://www.selenium.dev/downloads')
     browser.find_element(By.XPATH, "//a[contains(@href,'https://github.com/SeleniumHQ/')]").click()
     browser.quit()
-
-
