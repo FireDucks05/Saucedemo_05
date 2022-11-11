008c4ccf90aaca756a13ac352c7e80f4b6ac63df
Lizzzzi <jelizaveta.stolce@gmail.com>
2022-11-11 15:44:30 -0700

    Create branch Zoya_Liza_L2

--- tests/test_zoya_liza.py
+++ tests/test_zoya_liza.py
@@ -1,7 +1,5 @@
 from selenium.webdriver.common.by import By
-import pytest
 import time
-from webbrowser import BaseBrowser
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -10,15 +8,10 @@ INVENTORY_LINK = 'https://www.saucedemo.com/inventory.html'
 login_txt = 'standard_user'
 password_txt = 'secret_sauce'
 
+
 driver = webdriver.Chrome(ChromeDriverManager().install())
+
 
 driver.get("https://www.saucedemo.com/")
 time.sleep(5)
 
-
-def test_login():
-    driver.get(HOME_LINK)
-    driver.find_element(By.ID, 'user-name').send_keys(login_txt)
-    driver.find_element(By.ID, 'password').send_keys(password_txt)
-    driver.find_element(By.ID, 'login-button').click()
-    assert driver.current_url == INVENTORY_LINK
