e7ba3eabb00ef2e4b71cca71b1361f95d218cc1f
Liza Shtolts <75668303+Lizzzzi@users.noreply.github.com>
2022-11-11 17:26:16 -0700

    Create branch Zoya_Liza_L2_3 (#16)

--- tests/test_zoya_liza.py
+++ tests/test_zoya_liza.py
@@ -8,9 +8,12 @@ INVENTORY_LINK = 'https://www.saucedemo.com/inventory.html'
 login_txt = 'standard_user'
 password_txt = 'secret_sauce'
 
+HOME_LINK_2 = "https://www.saucedemo.com/"
+INVENTORY_LINK_2 = 'https://www.saucedemo.com/inventory.html'
+login_txt_2 = 'standard_user'
+password_txt_2 = 'secret_sauce'
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
-
 
 driver.get("https://www.saucedemo.com/")
 time.sleep(5)
