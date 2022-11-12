172610ec6edef7972aa34e8e065e8f50fc4cc9fc
Lizzzzi <jelizaveta.stolce@gmail.com>
2022-11-11 17:21:01 -0700

    Create branch Zoya_Liza_L2

--- tests/test_zoya_liza.py
+++ tests/test_zoya_liza.py
@@ -1,4 +1,3 @@
-from selenium.webdriver.common.by import By
 import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -8,10 +7,10 @@ INVENTORY_LINK = 'https://www.saucedemo.com/inventory.html'
 login_txt = 'standard_user'
 password_txt = 'secret_sauce'
 
-
 driver = webdriver.Chrome(ChromeDriverManager().install())
-
 
 driver.get("https://www.saucedemo.com/")
 time.sleep(5)
+
+
 
