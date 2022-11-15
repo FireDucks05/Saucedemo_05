6518dbe5e9b55226a9aec507c890ac4337a383cc
KateFox <52407432+rgirl0801@users.noreply.github.com>
2022-11-14 22:25:48 -0500

    add steps

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -15,14 +15,16 @@ class TestCartClass:
         self.inventory_page = InventoryPage(browser, url + 'inventory.html')
 
 
-    @allure.story('US_002.00')
-    @allure.step('TC_002.00.01')
+    @allure.epic('US_002.00')
+    @allure.story('TC_002.00.02')
     @allure.title("Change bage when adding")
     def test_change_bage_when_adding(self, browser, url):
-        self.login_page.login_ui(POSITIVE_LOGIN_CREDENTIALS[0][0], POSITIVE_LOGIN_CREDENTIALS[0][1])
-
-        self.inventory_page.add_to_cart()
-        self.inventory_page.bage_has_changed()
+        with allure.step('step1 login'):
+            self.login_page.login_ui(POSITIVE_LOGIN_CREDENTIALS[0][0], POSITIVE_LOGIN_CREDENTIALS[0][1])
+        with allure.step('step2 add to cart'):
+            self.inventory_page.add_to_cart()
+        with allure.step('step3 changes'):
+            self.inventory_page.bage_has_changed()
 
     @allure.story('US_002.00')
     @allure.step('TC_002.00.02')
