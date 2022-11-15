889a7cf5ff90e0bc4aa7710ef5e9e92ffec5523d
KateFox <52407432+rgirl0801@users.noreply.github.com>
2022-11-15 15:45:05 -0500

    add steps

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -16,8 +16,8 @@ class TestCartClass:
 
 
     @allure.epic('US_002.00')
-    @allure.story('TC_002.00.02')
-    @allure.title("Change bage when adding")
+    @allure.story('TC_002.00.01')
+    @allure.title("Bage change when adding")
     def test_change_bage_when_adding(self, browser, url):
         with allure.step('step1 login'):
             self.login_page.login_ui(POSITIVE_LOGIN_CREDENTIALS[0][0], POSITIVE_LOGIN_CREDENTIALS[0][1])
@@ -28,10 +28,13 @@ class TestCartClass:
 
     @allure.story('US_002.00')
     @allure.step('TC_002.00.02')
-    @allure.title("Change button text")
+    @allure.title("Button text after remove")
     def test_change_add_button_text(self, browser, url):
-        self.login_page.login_ui(POSITIVE_LOGIN_CREDENTIALS[0][0], POSITIVE_LOGIN_CREDENTIALS[0][1])
-
-        self.inventory_page.button_text_before_adding()
-        self.inventory_page.add_to_cart()
-        self.inventory_page.button_text_after_adding()
+        with allure.step('step1 login'):
+            self.login_page.login_ui(POSITIVE_LOGIN_CREDENTIALS[0][0], POSITIVE_LOGIN_CREDENTIALS[0][1])
+        with allure.step('step2 text before adding'):
+            self.inventory_page.button_text_before_adding()
+        with allure.step('step3 click on button'):
+            self.inventory_page.add_to_cart()
+        with allure.step('step4 text after adding'):
+            self.inventory_page.button_text_after_adding()
