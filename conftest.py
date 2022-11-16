7acfa5574a07a3db96ac9e549b445b20bc21a0b5
KateFox <52407432+rgirl0801@users.noreply.github.com>
2022-11-16 15:28:38 -0500

    fixed browser

--- conftest.py
+++ conftest.py
@@ -1,30 +1,36 @@
 import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+
 
 from constants import VALID_BROWSERS, COMMAND_EXECUTOR
 
 
-# @pytest.fixture(autouse=True)
-# def browser(request):
-#     browser = request.config.getoption("--browser")
-#     if browser == "firefox":
-#         options = webdriver.FirefoxOptions()
-#         options.add_argument("--window-size=1600,1080")
-#         options.headless = True
-#
-#         browser = webdriver.Firefox(
-#             service=FirefoxService(GeckoDriverManager().install()), options=options
-#         )
-#         yield browser
-#         browser.quit()
-#     else:
-#         options = webdriver.ChromeOptions()
-#         options.add_argument("--window-size=1600,1080")
-#         options.headless = False
-#         browser = webdriver.Chrome(
-#             service=Service(ChromeDriverManager().install()), options=options
-#         )
-#         yield browser
-#         browser.quit()
+@pytest.fixture(autouse=True)
+def browser(request):
+    browser = request.config.getoption("--launch")
+    if browser == "firefox":
+        options = webdriver.FirefoxOptions()
+        options.add_argument("--window-size=1600,1080")
+        options.headless = True
+
+        browser = webdriver.Firefox(
+            service=FirefoxService(GeckoDriverManager().install()), options=options
+        )
+        yield browser
+        browser.quit()
+    else:
+        options = webdriver.ChromeOptions()
+        options.add_argument("--window-size=1600,1080")
+        options.headless = True
+        browser = webdriver.Chrome(
+            service=Service(ChromeDriverManager().install()), options=options
+        )
+        yield browser
+        browser.quit()
 
 
 @pytest.fixture(autouse=True)
@@ -39,13 +45,3 @@ def pytest_addoption(parser):
     parser.addoption("--launch", default="firefox", choices=["chrome", "firefox", "ci"])
 
 
-@pytest.fixture(autouse=True)
-def browser(request):
-    launch = request.config.getoption("--launch")
-    if launch == 'ci':
-        browser = VALID_BROWSERS["remote"](command_executor=COMMAND_EXECUTOR["ci"])
-    else:
-        browser = VALID_BROWSERS[launch]()
-        browser.maximize_window()
-    yield browser
-    browser.quit()
