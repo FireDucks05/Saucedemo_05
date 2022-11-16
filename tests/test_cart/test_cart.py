1014eda45364a91a753e0460bbc070c34ac8d95c
KateFox <52407432+rgirl0801@users.noreply.github.com>
2022-11-15 19:02:28 -0500

    add fixture with login

--- tests/test_cart/test_cart.py
+++ tests/test_cart/test_cart.py
@@ -1,17 +1,13 @@
 import allure
 import pytest
 
-from constants import POSITIVE_LOGIN_CREDENTIALS
 from pages.inventory_page import InventoryPage
-from pages.login_page import LoginPage
 
 
 class TestCartClass:
 
     @pytest.fixture(autouse=True)
     def setup(self, browser, url):
-        self.login_page = LoginPage(browser, url)
-        self.login_page.open_page()
         self.inventory_page = InventoryPage(browser, url + 'inventory.html')
 
 
@@ -19,22 +15,18 @@ class TestCartClass:
     @allure.story('TC_002.00.01')
     @allure.title("Bage change when adding")
     def test_change_bage_when_adding(self, browser, url):
-        with allure.step('step1 login'):
-            self.login_page.login_ui(POSITIVE_LOGIN_CREDENTIALS[0][0], POSITIVE_LOGIN_CREDENTIALS[0][1])
-        with allure.step('step2 add to cart'):
+        with allure.step('step1 add to cart'):
             self.inventory_page.add_to_cart()
-        with allure.step('step3 changes'):
+        with allure.step('step2 watch changes'):
             self.inventory_page.bage_has_changed()
 
     @allure.epic('US_002.00')
     @allure.story('TC_002.00.02')
     @allure.title("Button text after remove")
     def test_change_add_button_text(self, browser, url):
-        with allure.step('step1 login'):
-            self.login_page.login_ui(POSITIVE_LOGIN_CREDENTIALS[0][0], POSITIVE_LOGIN_CREDENTIALS[0][1])
-        with allure.step('step2 text before adding'):
+        with allure.step('step1 text before adding'):
             self.inventory_page.button_text_before_adding()
-        with allure.step('step3 click on button'):
+        with allure.step('step2 click on button'):
             self.inventory_page.add_to_cart()
-        with allure.step('step4 text after adding'):
+        with allure.step('step3 text after adding'):
             self.inventory_page.button_text_after_adding()
