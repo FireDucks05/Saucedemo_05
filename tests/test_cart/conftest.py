6c6e52eeb9f0e1c6ef86f16d63dff8ba184b2c46
KateFox <52407432+rgirl0801@users.noreply.github.com>
2022-11-16 10:53:18 -0500

    add fixture with login

--- tests/test_cart/conftest.py
+++ tests/test_cart/conftest.py
@@ -1,14 +1,11 @@
-import allure
 import pytest
 
-from constants import POSITIVE_LOGIN_CREDENTIALS, NEGATIVE_LOGIN_CREDENTIALS
+from constants import POSITIVE_LOGIN_CREDENTIALS
 from pages.login_page import LoginPage
+
 
 @pytest.fixture(autouse=True)
 def login_standart(browser, url):
     login_page = LoginPage(browser, url)
     login_page.open_page()
     login_page.login_ui(POSITIVE_LOGIN_CREDENTIALS[0][0], POSITIVE_LOGIN_CREDENTIALS[0][1])
-    # assert login_page.page_is_open(
-    #     url="https://www.saucedemo.com/inventory.html"
-    # )
