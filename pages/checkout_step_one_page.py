86b0d20b6289372122582e110769cb905167d2fa
KateFox <52407432+rgirl0801@users.noreply.github.com>
2022-11-17 20:39:57 -0500

    add US007 doesn't work as expect

--- pages/checkout_step_one_page.py
+++ pages/checkout_step_one_page.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium.webdriver.common.by import By
 
 from pages.base_page import BasePage
@@ -12,8 +14,11 @@ class CheckoutStepOnePage(BasePage):
 
     def input_your_informarion(self, first_name: str, last_name: str, zip :str):
         self.wait_until_clickable(self.FIRST_NAME_BUTTON).send_keys(first_name)
+        time.sleep(3)
         self.wait_until_clickable(self.LAST_NAME_BUTTON).send_keys(last_name)
+        time.sleep(3)
         self.wait_until_clickable(self.POSTAL_CODE_BUTTON).send_keys(zip)
+        time.sleep(3)
         self.wait_until_clickable(self.CONTINUE_BUTTON).click()
 
 
--- tests/test_cart/test_checkout.py
+++ tests/test_cart/test_checkout.py
@@ -1,3 +1,5 @@
+import time
+
 import allure
 import pytest
 
@@ -13,18 +15,36 @@ class TestCheckoutClass:
     def setup(self, browser, url):
         self.inventory_page = InventoryPage(browser, url + 'inventory.html')
         self.cart_page = CartPage(browser, url + 'cart.html')
-        self.chekout_page = CheckoutStepOnePage(browser, url + 'checkout-step-one.html')
+        self.checkout_page = CheckoutStepOnePage(browser, url + 'checkout-step-one.html')
 
 
-    @allure.epic('UC_006.00.01.00')
+    @allure.epic('UC_006.00')
     @allure.story('TC_006.00.01')
     @allure.title("Successfully purchase")
     def test_purchase(self, browser, url):
         with allure.step('step1 add to cart'):
             self.inventory_page.add_to_cart()
+        with allure.step('step2 go to cart'):
             self.inventory_page.go_to_cart()
 
+        with allure.step('step3 go chekout'):
             self.cart_page.checkout()
 
-            self.chekout_page.input_your_informarion(
+        with allure.step('step4 fill information'):
+            self.checkout_page.input_your_informarion(
                 INFORMATION_DATA["first_name"], INFORMATION_DATA["last_name"], INFORMATION_DATA["zip"])
+
+    @allure.epic('UC_007.00')
+    @allure.story('TC_007.00.01')
+    @allure.title("Invalid message on checkout step")
+    def test_error_message(self, browser, url):
+        with allure.step('step1 add to cart'):
+            self.inventory_page.add_to_cart()
+        with allure.step('step2 go to cart'):
+            self.inventory_page.go_to_cart()
+
+        with allure.step('step3 go chekout'):
+            self.cart_page.checkout()
+
+        with allure.step('step4 fill information without first name'):
+            self.checkout_page.input_your_informarion('', INFORMATION_DATA["last_name"], INFORMATION_DATA["zip"])
