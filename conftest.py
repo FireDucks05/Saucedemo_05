0b47fa8e825ab3d59416575c8c1fa6c3708e2321
KateFox <52407432+rgirl0801@users.noreply.github.com>
2022-11-19 21:01:02 -0500

    simple conftest

--- conftest.py
+++ conftest.py
@@ -1,35 +1,22 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 
-
+'''Initializing browser'''
 @pytest.fixture(autouse=True)
 def browser(request):
     browser = request.config.getoption("--browser")
-    if browser == "firefox":
-        options = webdriver.FirefoxOptions()
-        options.add_argument("--window-size=1600,1080")
-        options.headless = True
+    options = webdriver.FirefoxOptions()
+    options.add_argument("--window-size=1600,1080")
+    options.headless = True
+    browser = webdriver.Firefox(
+        service=FirefoxService(GeckoDriverManager().install()), options=options
+    )
+    yield browser
+    browser.quit()
 
-        browser = webdriver.Firefox(
-            service=FirefoxService(GeckoDriverManager().install()), options=options
-        )
-        yield browser
-        browser.quit()
-    else:
-        options = webdriver.ChromeOptions()
-        options.add_argument("--window-size=1600,1080")
-        options.headless = True
-        browser = webdriver.Chrome(
-            service=Service(ChromeDriverManager().install()), options=options
-        )
-        yield browser
-        browser.quit()
-
-
+'''Add URL'''
 @pytest.fixture(autouse=True)
 def url():
     url = "https://www.saucedemo.com/"
@@ -37,6 +24,12 @@ def url():
         raise Exception("Wrong environment")
     return url
 
-
+'''Opportunity to launch background mode'''
 def pytest_addoption(parser):
-    parser.addoption("--browser", default="chrome", choices=["chrome", "firefox"])
+    parser.addoption("--headless", action="store_true", help="headless mode on or off, --headless")
+
+
+'''System fixture for string parsing'''
+@pytest.fixture(scope='class')
+def headless(request):
+    return request.config.getoption("--headless")
