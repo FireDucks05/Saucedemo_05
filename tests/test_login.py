d178ed7cbf747a900e005b684c6c7a8a1cc40036
KateFox <52407432+rgirl0801@users.noreply.github.com>
2022-11-23 19:46:08 -0500

    login page marks fixed

--- tests/test_login.py
+++ tests/test_login.py
@@ -17,18 +17,19 @@ class TestAuthorizationClass:
         POSITIVE_LOGIN_CREDENTIALS,
         ids=["standart_user", "problem_user", "performance_glitch_user"],
     )
-    @allure.story('US_001.00')
-    @allure.title("Positive login")
+    @allure.story('US_001.00 | Login Page')
+    @allure.title("Registered user is able to login with valid credentials")
     def test_login_positive(self, user, password):
         self.login_page.login_ui(user, password)
         assert self.login_page.page_is_open(
             url="https://www.saucedemo.com/inventory.html"
         )
 
-    @allure.story('US_001.00')
-    @allure.title("Negative login")
+
+    @allure.story('US_001.00 | Login Page')
+    @allure.title("Lockout login")
     @pytest.mark.xfail()
-    def test_login_negative(self):
+    def test_login_lockout_use(self):
         self.login_page.login_ui(
             NEGATIVE_LOGIN_CREDENTIALS["user"], NEGATIVE_LOGIN_CREDENTIALS["password"]
         )
@@ -36,29 +37,8 @@ class TestAuthorizationClass:
             url="https://www.saucedemo.com/inventory.html"
         )
 
-    @allure.story('US_001.00')
-    @allure.title("Negative login")
-    @pytest.mark.xfail()
-    def test_login_negative(self):
-        self.login_page.login_ui(
-            NEGATIVE_LOGIN_CREDENTIALS["user"], NEGATIVE_LOGIN_CREDENTIALS["password"]
-        )
-        assert self.login_page.page_is_open(
-            url="https://www.saucedemo.com/inventory.html"
-        )
-
-    @allure.story('US_001.00')
-    @allure.title("Standart_user login")
-    def test_login_standart(self):
-        self.login_page.login_ui(
-            POSITIVE_LOGIN_CREDENTIALS[0][0], POSITIVE_LOGIN_CREDENTIALS[0][1]
-        )
-        assert self.login_page.page_is_open(
-            url="https://www.saucedemo.com/inventory.html"
-        )
-
-    @allure.story('US_001.00')
-    @allure.title("Standart_user login")
+    @allure.story('US_001.00 | Login Page')
+    @allure.title("Login without password impossible")
     @pytest.mark.negative
     def test_login_WO_pass(self):
         self.login_page.login_ui(
@@ -66,8 +46,8 @@ class TestAuthorizationClass:
         )
         self.login_page.impossibility_auth()
 
-    @allure.story('US_001.00')
-    @allure.title("Standart_user login")
+    @allure.story('US_001.00 | Login Page')
+    @allure.title("Login without user_name impossible")
     @pytest.mark.negative
     def test_login_WO_login(self):
         self.login_page.login_ui(
@@ -75,13 +55,13 @@ class TestAuthorizationClass:
         )
         self.login_page.impossibility_auth()
 
-    @allure.story('US_001.00')
-    @allure.title("Logout standart")
+    @allure.story('US_001.00 | Login Page')
+    @allure.title("Purpose to fail case")
     @pytest.mark.positive
-    def test_logout_standart(self):
+    def test_for_fail(self):
         self.login_page.login_ui(
             POSITIVE_LOGIN_CREDENTIALS[0][0], POSITIVE_LOGIN_CREDENTIALS[0][1]
         )
         self.login_page.burger_menu()
         self.login_page.logout()
-        assert self.login_page.page_is_open(url="https://www.saucedemo.com/")
+        assert self.login_page.page_is_open(url="https://www.saucedemo.com/inventory.html")
