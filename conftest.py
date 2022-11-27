b2e4f12c4f3aad2954b548cd9f580ace30efa701
Anton Bloha <qvotan@gmail.com>
2022-11-27 13:14:31 -0800

    fixed login logout from conftest test

--- conftest.py
+++ conftest.py
@@ -10,9 +10,27 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="function")
+def browser():
+    options = webdriver.ChromeOptions()
+    options.headless = False
+    browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
-def setup(request):
-    browser = webdriver.Chrome("/Users/abloha/selenium/chromedriver" )
+    return browser
+
+
+    #request.cls.browser = browser
+
+
+
+@pytest.fixture(autouse=True)
+def login(browser):
     browser.get("https://www.saucedemo.com/")
-    request.cls.browser = browser
+    browser.find_element(By.ID, 'user-name').send_keys('standard_user')
+    browser.find_element(By.ID, 'password').send_keys('secret_sauce')
+    browser.find_element(By.ID, 'login-button').click()
+    yield browser
+    browser.find_element(By.ID, "react-burger-menu-btn").click()
+    time.sleep(2)
+    browser.find_element(By.ID, "logout_sidebar_link").click()
+    browser.quit()
--- utilities/login_logout.py
+++ utilities/login_logout.py
@@ -4,16 +4,12 @@ from selenium.webdriver.common.by import By
 from conftest import *
 
 
-@pytest.mark.usefixtures("setup")
 @pytest.fixture(scope="function")
 def login(setup):
     browser.find_element(By.ID, 'user-name').send_keys('standard_user')
     browser.find_element(By.ID, 'password').send_keys('secret_sauce')
     browser.find_element(By.ID, 'login-button').click()
-
-
-@pytest.fixture(scope="function")
-def logout(setup):
+    yield
     browser.find_element(By.ID, "react-burger-menu-btn").click()
     time.sleep(2)
     browser.find_element(By.ID, "logout_sidebar_link").click()
