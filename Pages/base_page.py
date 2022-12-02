27ffd50cdf3c0d4f407d0b14b8b479f9cc7b4440
znov2 <znov2017@gmail.com>
2022-12-02 08:53:44 -0800

    test driver after fix "page entry"

--- Pages/base_page.py
+++ Pages/base_page.py
@@ -1,3 +1,7 @@
+from selenium.webdriver.support.ui import WebDriverWait as Wait
+from selenium.webdriver.support import expected_conditions as EC
+
+
 class BasePage:
     def __init__(self, driver, url):
         self.driver = driver
@@ -5,3 +9,12 @@ class BasePage:
 
     def open(self):
         self.driver.get(self.url)
+
+    def element_is_visible(self, locator, timeout=5):
+        return Wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+
+    def element_are_visible(self, locator, timeout=5):
+        return Wait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
+
+
+
--- Pages/form_page.py
+++ Pages/form_page.py
@@ -1,5 +1,20 @@
+from selenium.webdriver import Keys
 from Pages.base_page import BasePage
+from Pages.form_page_locators import FormPageLocators as Locators
+import time
+
 
 class FormPage(BasePage):
+
     def fill_field_and_submit(self):
-        pass
+        username = "standard_user"
+        password = "secret_sauce"
+        self.element_is_visible(Locators.USER_NAME).send_keys(username)
+        self.element_is_visible(Locators.PASSWORD).send_keys(password)
+        self.element_is_visible(Locators.LOGIN).click()
+        return username, password
+
+    def form_result(self):
+        result_list = self.element_are_visible(Locators.RESULT_TABLE)
+        result_text = [i.text for i in result_list]
+        return result_text
