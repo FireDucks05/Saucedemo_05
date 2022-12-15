ea9b79a9487559e98441c639576b76cafbede967
Anton Bloha <qvotan@gmail.com>
2022-12-15 15:54:19 -0800

    worked on login logout wrapper

--- conftest.py
+++ conftest.py
@@ -1,6 +1,9 @@
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from pages import login_logout_page
+
+
 
 browser = None
 homepage = "https://www.saucedemo.com/"
@@ -23,15 +26,7 @@ def setup(request):
 
 @pytest.fixture(autouse=True)
 def login(browser):
-    browser.get("https://www.saucedemo.com/")
-    browser.find_element(By.ID, 'user-name').send_keys('standard_user')
-    browser.find_element(By.ID, 'password').send_keys('secret_sauce')
-    browser.find_element(By.ID, 'login-button').click()
-    yield browser
-    browser.find_element(By.ID, "react-burger-menu-btn").click()
-    time.sleep(2)
-    browser.find_element(By.ID, "logout_sidebar_link").click()
-    browser.quit()
+    def getlogin()
 
 
 
