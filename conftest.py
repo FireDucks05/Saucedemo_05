5f169deca3c537291a2b2dac3b71369d7de4f144
Anton Bloha <qvotan@gmail.com>
2022-12-15 10:48:00 -0800

    worked on login logout wrapper

--- conftest.py
+++ conftest.py
@@ -21,9 +21,17 @@ def setup(request):
 
 
 
-
-
-
+@pytest.fixture(autouse=True)
+def login(browser):
+    browser.get("https://www.saucedemo.com/")
+    browser.find_element(By.ID, 'user-name').send_keys('standard_user')
+    browser.find_element(By.ID, 'password').send_keys('secret_sauce')
+    browser.find_element(By.ID, 'login-button').click()
+    yield browser
+    browser.find_element(By.ID, "react-burger-menu-btn").click()
+    time.sleep(2)
+    browser.find_element(By.ID, "logout_sidebar_link").click()
+    browser.quit()
 
 
 
